e112c0c7cd584681a9f23ea34c90ed798c546c57
deyveson <deyvesonn@outlook.com>
2018-10-16 13:55:16 -0400

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,9 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component' 
 
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-car.service';
-import { OrderComponent } from './order/order.component'
+import { OrderComponent } from './order/order.component';
+import { InputComponent } from 'app/shared/input/input.component';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +37,8 @@ import { OrderComponent } from './order/order.component'
     MenuItemComponent,
     ShoppingCartComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
